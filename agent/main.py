127c58c39271a018b05eefb0837b18dab2ccc9de
Luke Goodwin <lukegoodwin1477@outlook.com>
2024-05-19 12:22:51 +0100

    refactor flask app

--- agent/main.py
+++ agent/main.py
@@ -6,55 +6,68 @@ from PIL import Image
 import numpy as np
 import io
 
-img_height = 256
-img_width = 256
+# Constants
+IMG_HEIGHT = 256
+IMG_WIDTH = 256
 
-# Load the models
-graphic_style_model = tf.keras.models.load_model('models/Graphic-Style-classifier.keras')
-graphic_style_classes = ['Cartoon', 'Fantasy', 'Low-Poly', 'Pixel-Art', 'Realistic']
+# Load models and their classes
+models_info = {
+    "graphic_style": {
+        "model": load_model('models/Graphic-Style-classifier-best-model.keras'),
+        "classes": ['Cartoon', 'Fantasy', 'Low-Poly', 'Pixel-Art', 'Realistic'],
+    },
+    "perspective": {
+        "model": load_model('models/Perspective-classifier-best-model.keras'),
+        "classes": ['First-Person', 'Isometric', 'Third-Person', 'Top-Down'],
+    },
+    "game": {
+        "model": load_model('models/Game-classifier-best-model.keras'),
+        "classes": ['BattleBit', 'CounterStrike', 'DarkSouls', 'EuropaUniversalis', 'Fortnite', 'GrandTheftAuto', 'Grayzone', 'Hades', 'HeartsOfIron', 'LeagueOfLegends', 'LethalCompany', 'Minecraft', 'Overwatch', 'RuneScape', 'Rust', 'SeaOfThieves', 'Terraria', 'TotalWar', 'VRising', 'Warno'],
+    },
+}
 
-perspective_model = tf.keras.models.load_model('models/Perspective-classifier.keras')
-perspective_classes = ['First-Person', 'Isometric', 'Third-Person', 'Top-Down']
-
-game_model = tf.keras.models.load_model('models/Game-classifier.keras')
-game_classes = ['BattleBit', 'CounterStrike', 'DarkSouls', 'EuropaUniversalis', 'Fortnite', 'GrandTheftAuto', 'Grayzone', 'Hades', 'HeartsOfIron', 'LeagueOfLegends', 'LethalCompany', 'Minecraft', 'Overwatch', 'RuneScape', 'Rust', 'SeaOfThieves', 'Terraria', 'TotalWar', 'VRising', 'Warno']
-
-# Preprocess the image
+# Image preprocessing
 def process_image(image):
     if image.mode != "RGB":
         image = image.convert("RGB")
-    image = image.resize((img_height, img_width))
+    image = image.resize((IMG_HEIGHT, IMG_WIDTH))
     image = img_to_array(image)
     image = np.expand_dims(image, axis=0)
     return image
 
-# Create a Flask app
+# Predict function
+def predict_image(image, model_info):
+    predictions = model_info["model"].predict(image)
+    score = tf.nn.softmax(predictions[0])
+    predicted_class = model_info["classes"][np.argmax(score)]
+    confidence = 100 * np.max(score)
+    return f"This image most likely belongs to {predicted_class} with a {confidence:.2f} percent confidence."
+
+# Create Flask app
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         if 'file' not in request.files:
-            return 'No file part'
-        file = request.files['file'].read()
-        image = Image.open(io.BytesIO(file))
-        img = image.resize((img_height, img_width))
-        img_array = tf.keras.preprocessing.image.img_to_array(img)
-        img_array = tf.expand_dims(img_array, 0)  # Create a batch
+            return 'No file part', 400
+        file = request.files['file']
+        if file.filename == '':
+            return 'No selected file', 400
+        try:
+            image = Image.open(io.BytesIO(file.read()))
+            img_array = process_image(image)
 
-        graphic_predictions = graphic_style_model.predict(img_array)
-        graphic_score = tf.nn.softmax(graphic_predictions[0])
-        graphic_result = "This image most likely belongs to {} with a {:.2f} percent confidence.".format(graphic_style_classes[np.argmax(graphic_score)], 100 * np.max(graphic_score))
-        
-        game_predictions = game_model.predict(img_array)
-        game_score = tf.nn.softmax(game_predictions[0])
-        game_result = "This image most likely belongs to {} with a {:.2f} percent confidence.".format(game_classes[np.argmax(game_score)], 100 * np.max(game_score))
-        
-        perspective_predictions = perspective_model.predict(img_array)
-        perspective_score = tf.nn.softmax(perspective_predictions[0])
-        perspective_result = "This image most likely belongs to {} with a {:.2f} percent confidence.".format(perspective_classes[np.argmax(perspective_score)], 100 * np.max(perspective_score))
-        
-        return render_template('prediction.html', graphic_prediction=graphic_result, game_prediction=game_result, perspective_prediction=perspective_result)
+            # Predictions
+            graphic_result = predict_image(img_array, models_info["graphic_style"])
+            game_result = predict_image(img_array, models_info["game"])
+            perspective_result = predict_image(img_array, models_info["perspective"])
+
+            return render_template('prediction.html', graphic_prediction=graphic_result, game_prediction=game_result, perspective_prediction=perspective_result)
+
+        except Exception as e:
+            return str(e), 500
+
     return render_template('index.html')
 
 if __name__ == '__main__':
